a7e7b5dc2b2c935d1266d5c043548be4e51f0eac
hilmiyahnafisahh <hilmiyahnafisahh@gmail.com>
2026-04-03 06:52:04 +0700

    resource barang dan create helper

--- app/Models/barang.php
+++ app/Models/barang.php
@@ -4,7 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use illuminate\support\Facades\DB;
+// tambahan
+use Illuminate\Support\Facades\DB;
+
+
 
 class barang extends Model
 {
@@ -12,22 +15,26 @@ class barang extends Model
     protected $primaryKey = 'id';
     protected $fillable = ['id_barang', 'nama_barang', 'stok', 'satuan', 'harga_beli', 'harga_jual'];
 
-    public static function getkodebarang()
+    public static function getKodeBarang()
     {
-        // query untuk mendapatkan kode kategori terakhir
-        $sql = "SELECT IFNULL(MAX(kode_barang), 'BRG000') AS kode_barang FROM barang";
-        $kd_barang = DB::select($sql); //menjalankan query dan menyimpan hasilnya dalam variabel $result
+        // query kode perusahaan
+        $sql = "SELECT IFNULL(MAX(id_barang), 'BRG000') as kode_barang 
+                FROM barang ";
+        $kodebarang = DB::select($sql);
 
-        //cacah hasil query untuk mendapatkan kode kategori terakhir
-        foreach ($kd_barang as $kdbrg) {
-            $kd = $kdbrg->kode_barang; //mengambil nilai kode_kategori dari hasil query
+        // cacah hasilnya
+        foreach ($kodebarang as $kdbrg) {
+            $kd = $kdbrg->kode_barang;
         }
-
-        //proses untuk membuat kode kategori baru dengan format "KAT" diikuti dengan angka yang diambil dari kode kategori terakhir
-        $kdawal= substr($kd,-3); //mengambil 3 karakter terakhir dari kode kategori terakhir
-        $kdawal++; //menambahkan 1 pada angka yang diambil dari kode kategori terakhir
-        //atau $kdakhir=$kdawal+1
-        $kdakhir= 'BRG'.str_pad($kdawal,3,"0",STR_PAD_LEFT); //menggabungkan "KAT" dengan angka yang sudah ditambahkan 1, dan memastikan bahwa angka tersebut memiliki 3 digit dengan menambahkan nol di depan jika diperlukan
-        return $kdakhir; //mengembalikan kode kategori baru yang sudah dibuat
+        // Mengambil substring tiga digit akhir dari string PR-000
+        $noawal = substr($kd,-3);
+        $noakhir = $noawal+1; //menambahkan 1, hasilnya adalah integer cth 1
+        $noakhir = 'BRG'.str_pad($noakhir,3,"0",STR_PAD_LEFT); //menyambung dengan string PR-001
+        return $noakhir;
+    }
+    public function setHargaBarangAttribute($value)
+    {
+        // Hapus koma (,) dari nilai sebelum menyimpannya ke database
+        $this->attributes['harga_barang'] = str_replace('.', '', $value);
     }
 }
