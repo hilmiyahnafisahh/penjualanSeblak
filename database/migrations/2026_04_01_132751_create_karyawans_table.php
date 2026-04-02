c119afcf8dfc19703c5597a20a1a723d56f84d83
HaniAzizahSyntia <hanisyntia@gmail.com>
2026-04-02 17:51:50 +0700

    karyawan

--- database/migrations/2026_04_01_132751_create_karyawans_table.php
+++ database/migrations/2026_04_01_132751_create_karyawans_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('karyawan', function (Blueprint $table) {
+            $table->id();
             $table->id('id_karyawan');
             $table->string('nama');
             $table->string('alamat');
