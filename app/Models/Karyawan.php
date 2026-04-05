821d329fcb434e3b2d8e79d21091d10174e1a672
asif050905 <muhammadasif05092005@gmail.com>
2026-04-05 13:25:42 +0700

    perubahan asif

--- app/Models/Karyawan.php
+++ app/Models/Karyawan.php
@@ -3,31 +3,28 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Facades\DB;
 
 class Karyawan extends Model
 {
     use HasFactory;
 
-    protected $table = 'karyawan';  
-
+    protected $table = 'karyawan';
     protected $guarded = [];
-    
-    public static function getKaryawanById($id_karyawan)
-    {  
-        // query kode perusahaan
-        $sql = "SELECT IFNULL(MAX(id_karyawan), 'KRY000') as id_karyawan 
-                FROM karyawan ";
-        $id_karyawan = DB::select($sql);
 
-        // cacah hasilnya
-        foreach ($id_karyawan as $idkry) {
+    public static function getkaryawankode($id_karyawan)
+    {
+        $sql = "SELECT IFNULL(MAX(id_karyawan), 'KRY000') as id_karyawan FROM karyawan";
+        $idkaryawan = DB::select($sql);
+
+        foreach ($idkaryawan as $idkry) {
             $id = $idkry->id_karyawan;
         }
-        // Mengambil substring tiga digit akhir dari string PR-000
-        $noawal = substr($id,-3);
-        $noakhir = $noawal+1; //menambahkan 1, hasilnya adalah integer cth 1
-        $noakhir = 'KRY'.str_pad($noakhir,3,"0",STR_PAD_LEFT); //menyambung dengan string PR-001
-        return $noakhir;
 
+        $nomawal = substr($id, -3);
+        $nomawal++;
+
+        return 'KRY' . str_pad($nomawal, 3, "0", STR_PAD_LEFT);
     }
 }
