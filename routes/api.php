956a9bf00ca7cf3caa0ee783510be1ad280f3726
PuttyAisyahDwiAngelina <puttyangelina7@gmail.com>
2026-05-13 20:34:31 +0700

    export catat beban

--- routes/api.php
+++ routes/api.php
@@ -4,21 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MidtransController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application.
-|
-*/
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// ========================================
-// CALLBACK / WEBHOOK MIDTRANS
-// ========================================
-
+// CALLBACK MIDTRANS
 Route::post('/midtrans/callback', [MidtransController::class, 'callback']);
