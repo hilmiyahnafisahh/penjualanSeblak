63c6840009d7a961ab22cf32c7d38d8be794a428
hilmiyahnafisahh <hilmiyahnafisahh@gmail.com>
2026-05-14 08:38:06 +0700

    penggajian form

--- database/migrations/2026_05_09_045401_create_menubarangs_table.php
+++ database/migrations/2026_05_09_045401_create_menubarangs_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('menubarang', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('id_menu')->constrained('menus')->onDelete('cascade');
+            $table->foreignId('id_menu')->constrained('menu')->onDelete('cascade');
             $table->foreignId('id_barang')->constrained('barang')->onDelete('cascade');
             $table->timestamps();
         });
