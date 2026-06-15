91fa217ec79ad2afa52953281bdb092e15ebf4d7
hilmiyahnafisahh <hilmiyahnafisahh@gmail.com>
2026-06-15 22:48:12 +0700

    registrasi

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ Route::get('/', function () {
 // // Untuk membuka halaman pembayaran beban
 // Route::get('/bayar-beban/{id}', [MidtransController::class, 'bayar'])
 //     ->name('beban.bayar');
-=======
+
 // ============================================================
 // Customer Auth (Login & Register)
 // ============================================================
